feea5317afd6c46604f17a9715d81de406a95d8c
pachikuriii <stakahashi0728@gmail.com>
2022-07-16 14:58:22 +0900

    引数がない場合に今月、今年の値が渡るように

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,24 +1,37 @@
 const argv = require('minimist')(process.argv.slice(2))
+const today = new Date()
+const thisYear = today.getFullYear()
+const thisMonth = today.getMonth() + 1
 
-const year = argv.y
-const month = argv.m
+let year
+if (argv.y === undefined) {
+  year = thisYear
+} else {
+  year = argv.y
+}
+
+let month
+if (argv.m === undefined) {
+  month = thisMonth
+} else {
+  month = argv.m
+}
 
 const lastDay = new Date(year, month, 0)
-const last_Day = lastDay.getDate()
+const lastDayNum = lastDay.getDate()
 const firstDay = new Date(year, month - 1, 2, -1)
-const first_Day = firstDay.getDate()
-let first_Wday = firstDay.getDay()
+const firstDayNum = firstDay.getDate()
+let firstWday = firstDay.getDay()
 
-title = month + '月' + year
+const title = month + '月' + year
 console.log(title.padStart(13, (' ')))
 console.log('日 月 火 水 木 金 土')
-process.stdout.write(''.padStart(first_Wday * 3))
+process.stdout.write(''.padStart(firstWday * 3))
 
-for (day = first_Day; day <= last_Day; day++) {
+for (let day = firstDayNum; day <= lastDayNum; day++) {
+  firstWday++
   day = (' ' + day).slice(-2)
-  first_Wday++
-
-  if (first_Wday % 7 === 0) {
+  if (firstWday % 7 === 0) {
     console.log(day)
   } else {
     process.stdout.write(String(day) + ' ')
