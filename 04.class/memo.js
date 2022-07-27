c91736b1d4bea9116f635a0af599beeed99a22fc
pachikuriii <stakahashi0728@gmail.com>
2022-07-27 16:57:45 +0900

    クラスを実装

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -10,32 +10,55 @@ const reader = require('readline').createInterface({
   output: process.stdout
 })
 
+class MemoPiece {
+  constructor (memoData) {
+    this.message = memoData[0]
+    this.value = uuidv4()
+    this.content = memoData.join('\n')
+  }
+}
+
+class Memo {
+  constructor () {
+    this.memosContent = memosContent
+  }
+
+  create (newMemo) {
+    this.memosContent.Memos.push(newMemo)
+    fs.writeFileSync('memo.json', JSON.stringify(this.memosContent))
+  }
+
+  show () {
+    for (let i = 0; i < this.memosContent.Memos.length; i++) {
+      console.log(this.memosContent.Memos[i].message)
+    }
+  }
+
+  detail (indexNum) {
+    console.log(this.memosContent.Memos[indexNum].content)
+  }
+
+  delete (indexNum) {
+    this.memosContent.Memos.splice(indexNum, 1)
+    fs.writeFileSync('memo.json', JSON.stringify(this.memosContent))
+  }
+}
+
 const memoData = []
 reader.on('line', (line) => {
   memoData.push(line)
 })
 reader.on('close', () => {
-  const newMemo =
-  {
-    message: memoData[0],
-    value: uuidv4(),
-    content: memoData.join('\n')
-  }
-  memosContent.Memos.push(newMemo)
-  const newMemos = JSON.stringify(memosContent)
-  fs.writeFileSync('memo.json', newMemos)
+  new Memo().create(new MemoPiece(memoData))
 })
 
 if (argv.l === true) {
-  for (let i = 0; i < memosContent.Memos.length; i++) {
-    console.log(memosContent.Memos[i].message)
-  }
-  process.stdin.pause()
+  new Memo(memosContent).show()
+  process.exit()
 } else if (argv.r === true) {
   const prompt = new Select({
     message: '表示したいメモを選択してください:',
     choices: memosContent.Memos
-
   })
   prompt.run()
     .then(answer => {
@@ -43,8 +66,7 @@ if (argv.l === true) {
       return indexNum
     })
     .then(indexNum => {
-      const fileContent = JSON.parse(fs.readFileSync('memo.json', 'utf8'))
-      console.log(fileContent.Memos[indexNum].content)
+      new Memo().detail(indexNum)
     })
     .catch(console.error)
 } else if (argv.d === true) {
@@ -58,9 +80,7 @@ if (argv.l === true) {
       return indexNum
     })
     .then(indexNum => {
-      const fileContent = JSON.parse(fs.readFileSync('memo.json', 'utf8'))
-      fileContent.Memos.splice(indexNum, 1)
-      fs.writeFileSync('memo.json', JSON.stringify(fileContent))
+      new Memo().delete(indexNum)
     })
     .then(answer =>
       console.log('メモが削除されました')
